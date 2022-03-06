aa55e7820b147de3dcfcafc9fbab0d4f5d4cdbf1
jkfitzpatrick <justin.fitzpatrick82@gmail.com>
2022-03-06 15:54:32 -0600

    starting coding challenge 1

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,89 @@
+/*
+let js = "amazing";
+console.log (40 + 8 + 23 - 10);
+console.log("Jonas");
+console.log(23);
+let firstName = "Jonas";
+console.log(firstName);
+*/
+
+// let javascriptIsFun = true;
+// console.log(javascriptIsFun);
+// console.log(typeof javascriptIsFun);
+// console.log(typeof true);
+// console.log(typeof "Jonas");
+
+// javascriptIsFun = "Yes!";
+// console.log(typeof javascriptIsFun)
+
+// let year;
+// console.log(typeof year);
+
+// let age = 30;  //declare variables that can change later using let
+// age = 31
+
+// const birthYear = 1991;
+// // birthYear = 1990;
+
+// var job = "programmer";
+// job = "teacher"
+
+// const now = 2037
+// const ageJonas = now - 1991;
+// const ageSarah =  now - 2018;
+// console.log(ageJonas, ageSarah);
+
+// console.log(ageJonas * 2, ageJonas/2, 2**3);
+// // 2**3 means 2 to the power of 3 = 2 * 2 * 2
+
+// const firstName = "Jonas";
+// const lastName = "Schmedtmann";
+// console.log(firstName+ " " + lastName);
+
+// let x = 10 + 5;
+// x += 10;
+// x *= 4;
+// console.log(x);
+
+// //Comparison oerators
+// console.log(ageJonas > ageSarah);
+// console.log(ageSarah >= 18);
+
+// const isFullAge = ageSarah >= 18;
+// console.log(now - 1991 > now - 2018);
+
+// const now = 2037;
+// const ageJonas = now - 1991;
+// const ageSarah = now - 2018;
+
+// console.log(now - 1991 > now - 2018);
+
+// console.log(25 - 10 - 5);
+// let x, y;
+// x = y = 25 - 10 - 5;
+// console.log(x,y);
+
+// const averageAge = (ageJonas + ageSarah)/2
+// console.log(averageAge);
+
+////////////////////////////////////////////////////
+// Coding Challenege #1
+
+/*
+Mark and John are trying to compare the BMI (Body Mass Index), which 
+is calculated using the formula:
+BMI = mass / height ** 2 = mass / (height * height).
+(mass in kg and height in meter).
+
+1. Store Mark's and John's mass and height in variable
+2. Calculate both their BMIs using the formula (you can even implement
+    both versions)
+3. Create a boolean veriable 'markHigherBMI' containing information aboiut
+whether Mark has a bigher BMI than John.
+
+TEST DATA !: Marks weights 78 kg and is 1.69 m tall.
+John weighs 92 kg and is 1.95 m tall.
+TEST DATA 2: Mark weighs 95 kg and is 1.88 m tall.
+John weighs 85 kg and is 1.76 m tall.
+*/
+
